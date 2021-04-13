9a121f9a1cbbce9e77fccd06c9954bc7558d6d1c
Veronica Ripper <vripper@alum.mit.edu>
2021-04-13 15:40:48 -0400

    import MbtaService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MbtaService } from './mbta.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,6 @@ export class AppComponent {
   routeArray = ['route1', 'route2'];
   stopArray = ['stop1', 'stop2'];
   directionArray = ['direction1', 'direction2'];
+
+  constructor(private mbtaService: MbtaService){}
 }
