691eaab5fd87619f8b097d7f70fc7950987a311d
Veronica Ripper <vripper@alum.mit.edu>
2021-04-13 14:49:47 -0400

    add dropdowns and remove unecessary template code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  routeArray = ['route1', 'route2'];
+  stopArray = ['stop1', 'stop2'];
+  directionArray = ['direction1', 'direction2'];
 }
